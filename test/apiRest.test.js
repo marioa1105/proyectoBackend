064b2b8e196643f047b5d9af8b3d8bb5ac4be8b1
marioa1105 <marioa1105@gmail.com>
2021-09-21 23:53:17 -0300

    desafio39_testApi

---
+++ test/apiRest.test.js
@@ -0,0 +1,27 @@
+const Producto = require ('../api/producto');
+const request = require('supertest')('http://localhost:8080');
+const expect = require('chai').expect;
+
+
+describe("test api rest",function(){
+    let id= "";
+    let total =0;
+    describe('POST', () => {
+        it("Verifica agregar",async function(){
+            let response = await request.post('/api/productos/guardar').send({title : "ProductoMocha",
+            price: 10,
+            thumbnail:"ImagenMocha"});
+            expect(response.status).to.eql(200);          
+        });
+    });
+    
+    describe('GET', () => {
+        it("Verificar listar",async function(){            
+            let response = await request.get('/api/productos/listar');
+            
+            expect(response.body.length).not.eql(0);
+        });
+    });
+    
+
+});
